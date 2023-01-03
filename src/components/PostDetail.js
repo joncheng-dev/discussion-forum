3be21611c273eb37314bc75091200431ebc2c531
Jonathan Cheng <jcheng461@gmail.com>
2023-01-03 16:58:47 +0900

    PostDetail component shows how long ago post was created. WIP: display this in PostList component

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -2,6 +2,33 @@ import React from "react";
 
 function PostDetail(props) {
   const { post, onDeleteClick } = props;
+
+  function timeSince(date) {
+    let seconds = Math.floor((new Date() - date) / 1000);
+    let interval = seconds / 31536000;
+
+    if (interval > 1) {
+      return Math.floor(interval) + " years ago";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months ago";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days ago";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+  }
+
   return (
     <React.Fragment>
       <h2>Post Detail</h2>
@@ -9,7 +36,7 @@ function PostDetail(props) {
       <h3>{post.title}</h3>
       <p>{post.text}</p>
       <p>
-        <em>Time Submitted: {post.timeSubmitted}</em>
+        <em>Post Created: {timeSince(post.timeSubmitted)}</em>
       </p>
       <hr />
       <button onClick={props.onEditClick}>Edit Ticket</button>
