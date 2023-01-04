daab922e7f03e322bcafc96a38522319405bf4c2
Jonathan Cheng <jcheng461@gmail.com>
2023-01-04 13:02:23 +0900

    refactor timeSince function to its own js file

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,5 @@
 import React from "react";
+import timeSince from "./time-since";
 
 function Post(props) {
   const divStyle = {
@@ -6,32 +7,6 @@ function Post(props) {
     backgroundColor: "#3A3B3C",
     paddingLeft: 10,
   };
-
-  function timeSince(date) {
-    let seconds = Math.floor((new Date() - date) / 1000);
-    let interval = seconds / 31536000;
-
-    if (interval > 1) {
-      return Math.floor(interval) + " years ago";
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-      return Math.floor(interval) + " months ago";
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-      return Math.floor(interval) + " days ago";
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-      return Math.floor(interval) + " hours ago";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-      return Math.floor(interval) + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
-  }
 
   return (
     <React.Fragment>
--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,33 +1,8 @@
 import React from "react";
+import timeSince from "./time-since";
 
 function PostDetail(props) {
   const { post, onDeleteClick } = props;
-
-  function timeSince(date) {
-    let seconds = Math.floor((new Date() - date) / 1000);
-    let interval = seconds / 31536000;
-
-    if (interval > 1) {
-      return Math.floor(interval) + " years ago";
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-      return Math.floor(interval) + " months ago";
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-      return Math.floor(interval) + " days ago";
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-      return Math.floor(interval) + " hours ago";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-      return Math.floor(interval) + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
-  }
 
   return (
     <React.Fragment>
