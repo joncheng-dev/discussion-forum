7d6d149e6f7b5d9ee48f75682d72f58f8e81e575
Jonathan Cheng <jcheng461@gmail.com>
2023-01-09 22:00:29 +0900

    pass test for combining Redux Reducers. Tests show each individual reducer matches default state of root reducer

---
+++ src/__tests__/reducers/index-reducer.test.js
@@ -0,0 +1,21 @@
+import rootReducer from "../../reducers/index";
+import { createStore } from "redux";
+import formVisibleReducer from "../../reducers/form-visible-reducer";
+import postListReducer from "../../reducers/post-list-reducer";
+
+let store = createStore(rootReducer);
+
+describe("rootReducer", () => {
+  test("Should return default state if no action type is recognized", () => {
+    expect(rootReducer({}, { type: null })).toEqual({
+      mainPostList: {},
+      formVisibleOnPage: false,
+    });
+  });
+  test("Check that initial state of postListReducer matches root reducer", () => {
+    expect(store.getState().mainPostList).toEqual(postListReducer(undefined, { type: null }));
+  });
+  test("Check that initial state of formVisibleReducer matches root reducer", () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+});
