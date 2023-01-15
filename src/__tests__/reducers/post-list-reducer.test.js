0b57748f3b9d483caa623f903e0f7b7b76f4a66e
Jonathan Cheng <jcheng461@gmail.com>
2023-01-15 13:50:30 +0900

    add key-value pairs for upvotes, downvotes, score -- to reducer tests

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -6,6 +6,9 @@ describe("postListReducer", () => {
     title: "How rare is this cat?",
     text: "Cat found with 5 toes on each paw!",
     timeSubmitted: 1672731107482,
+    upvotes: 0,
+    downvotes: 0,
+    score: 0,
     id: 1,
   };
 
@@ -14,12 +17,18 @@ describe("postListReducer", () => {
       title: "How rare is this cat?",
       text: "Cat found with 5 toes on each paw!",
       timeSubmitted: 1672731107482,
+      upvotes: 0,
+      downvotes: 0,
+      score: 0,
       id: 1,
     },
     2: {
       title: "Dog being derp!",
       text: "Dog in the snow",
       timeSubmitted: 1672731119244,
+      upvotes: 0,
+      downvotes: 0,
+      score: 0,
       id: 2,
     },
   };
@@ -29,12 +38,15 @@ describe("postListReducer", () => {
   });
 
   test("Should successfully add new post data to mainPostList", () => {
-    const { title, text, timeSubmitted, id } = postData;
+    const { title, text, timeSubmitted, upvotes, downvotes, score, id } = postData;
     action = {
       type: "ADD_POST",
       title: title,
       text: text,
       timeSubmitted: timeSubmitted,
+      upvotes: upvotes,
+      downvotes: downvotes,
+      score: score,
       id: id,
     };
     expect(postListReducer({}, action)).toEqual({
@@ -42,6 +54,9 @@ describe("postListReducer", () => {
         title: title,
         text: text,
         timeSubmitted: timeSubmitted,
+        upvotes: upvotes,
+        downvotes: downvotes,
+        score: score,
         id: id,
       },
     });
@@ -57,6 +72,9 @@ describe("postListReducer", () => {
         title: "Dog being derp!",
         text: "Dog in the snow",
         timeSubmitted: 1672731119244,
+        upvotes: 0,
+        downvotes: 0,
+        score: 0,
         id: 2,
       },
     });
