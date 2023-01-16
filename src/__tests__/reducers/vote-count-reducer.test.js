439e9e8fcd4cb75fa3f0d9dbe69fec821fb23be0
Jonathan Cheng <jcheng461@gmail.com>
2023-01-16 23:50:06 +0900

    voteCountReducer passes test - correctly takes DOWNVOTE action, returning downvote + 1, and score - 1.

--- src/__tests__/reducers/vote-count-reducer.test.js
+++ src/__tests__/reducers/vote-count-reducer.test.js
@@ -27,13 +27,35 @@ describe("voteCountReducer", () => {
       score: score,
       id: id,
     };
-    expect(voteCountReducer(score, action)).toEqual({
+    expect(voteCountReducer(postData, action)).toEqual({
       title: "How rare is this cat?",
       text: "Cat found with 5 toes on each paw!",
       timeSubmitted: 1672731107482,
       upvotes: 1,
       downvotes: 0,
       score: 4,
+      id: 1,
+    });
+  });
+  test("Should increase number of downvotes by one, decrease total score by 1, and update state slices appropriately", () => {
+    const { title, text, timeSubmitted, upvotes, downvotes, score, id } = postData;
+    action = {
+      type: "DOWNVOTE",
+      title: title,
+      text: text,
+      timeSubmitted: timeSubmitted,
+      upvotes: upvotes,
+      downvotes: downvotes,
+      score: score,
+      id: id,
+    };
+    expect(voteCountReducer(postData, action)).toEqual({
+      title: "How rare is this cat?",
+      text: "Cat found with 5 toes on each paw!",
+      timeSubmitted: 1672731107482,
+      upvotes: 0,
+      downvotes: 1,
+      score: 2,
       id: 1,
     });
   });
