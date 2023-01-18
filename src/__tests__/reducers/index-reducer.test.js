a6aa4f47cd6ca5bf4de7ad10a570c83f5239451d
Jonathan Cheng <jcheng461@gmail.com>
2023-01-18 23:51:57 +0900

    WIP: get voteCountReducer to pass test for both individual reducer and root reducer after properly handling action

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -44,4 +44,28 @@ describe("rootReducer", () => {
     store.dispatch(action);
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
   });
+  test("Check that UPVOTE action works for voteCountReducer and root reducer", () => {
+    const postData = {
+      title: "How rare is this cat?",
+      text: "Cat found with 5 toes on each paw!",
+      timeSubmitted: 1672731107482,
+      upvotes: 0,
+      downvotes: 0,
+      score: 3,
+      id: 1,
+    };
+    const { title, text, timeSubmitted, upvotes, downvotes, score, id } = postData;
+    const action = {
+      type: "UPVOTE",
+      title: title,
+      text: text,
+      timeSubmitted: timeSubmitted,
+      upvotes: upvotes,
+      downvotes: downvotes,
+      score: score,
+      id: id,
+    };
+    store.dispatch(action);
+    expect(store.getState().voteCountReducer).toEqual(voteCountReducer(undefined, action));
+  });
 });
