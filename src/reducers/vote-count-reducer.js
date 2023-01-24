af4526fe3efa1c8b6e43483e6d8ad830a0cf76e6
Jonathan Cheng <jcheng461@gmail.com>
2023-01-24 21:29:11 +0900

    refactor vote count reducer to only access state slices needed

--- src/reducers/vote-count-reducer.js
+++ src/reducers/vote-count-reducer.js
@@ -1,24 +1,16 @@
 const reducer = (state = {}, action) => {
-  const { title, text, timeSubmitted, upvotes, downvotes, score, id } = action;
+  const { upvotes, downvotes, score, id } = action;
   switch (action.type) {
     case "UPVOTE":
       return {
         ...state,
-        title: title,
-        text: text,
-        timeSubmitted: timeSubmitted,
         upvotes: upvotes + 1,
-        downvotes: downvotes,
         score: score + 1,
         id: id,
       };
     case "DOWNVOTE":
       return {
         ...state,
-        title: title,
-        text: text,
-        timeSubmitted: timeSubmitted,
-        upvotes: upvotes,
         downvotes: downvotes + 1,
         score: score - 1,
         id: id,
@@ -29,3 +21,35 @@ const reducer = (state = {}, action) => {
 };
 
 export default reducer;
+
+// const reducer = (state = {}, action) => {
+//   const { title, text, timeSubmitted, upvotes, downvotes, score, id } = action;
+//   switch (action.type) {
+//     case "UPVOTE":
+//       return {
+//         ...state,
+//         title: title,
+//         text: text,
+//         timeSubmitted: timeSubmitted,
+//         upvotes: upvotes + 1,
+//         downvotes: downvotes,
+//         score: score + 1,
+//         id: id,
+//       };
+//     case "DOWNVOTE":
+//       return {
+//         ...state,
+//         title: title,
+//         text: text,
+//         timeSubmitted: timeSubmitted,
+//         upvotes: upvotes,
+//         downvotes: downvotes + 1,
+//         score: score - 1,
+//         id: id,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export default reducer;
