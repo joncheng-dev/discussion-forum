97cd848268232c66c6b72ae6860e8560c68bcaf7
Jonathan Cheng <jcheng461@gmail.com>
2023-02-17 23:46:58 +0900

    WIP: refactor to use action creators and constants

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,9 @@
+// Post List Reducer
+export const ADD_POST = "ADD_POST";
+export const DELETE_POST = "DELETE_POST";
+export const UPVOTE = "UPVOTE";
+export const DOWNVOTE = "DOWNVOTE";
+// Form Visible Reducer
+export const TOGGLE_FORM = "TOGGLE_FORM";
+// Form Edit Reducer
+export const EDIT_FORM_TOGGLE = "EDIT_FORM_TOGGLE";
