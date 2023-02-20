d82b3926d697d0e5d4ef01ec4c34d922a67c1ef5
Jonathan Cheng <jcheng461@gmail.com>
2023-02-20 18:34:05 +0900

    upvote reducer passes test as part of root reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -38,6 +38,21 @@ describe("rootReducer", () => {
     store.dispatch(action);
     expect(store.getState().mainPostList).toEqual(postListReducer(undefined, action));
   });
+  test("Check that ADD_POST action works for postListReducer and root reducer - 2nd post", () => {
+    const action = {
+      type: "ADD_POST",
+      title: "Puppy sitting in the snow",
+      text: "Adorable golden retriever puppy",
+      timeSubmitted: 1672731119244,
+      upvotes: 0,
+      downvotes: 0,
+      score: 0,
+      id: 2,
+      imageUrl: "https://img.freepik.com/free-photo/shot-adorable-white-golden-retriever-puppy-sitting-snow_181624-44122.jpg",
+    };
+    store.dispatch(action);
+    expect(store.getState().mainPostList).toEqual(postListReducer(store.getState().mainPostList, action));
+  });
   test("Check that TOGGLE_FORM action works for formVisibleReducer and root reducer", () => {
     const action = {
       type: "TOGGLE_FORM",
@@ -46,29 +61,36 @@ describe("rootReducer", () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
   });
   test("Check that UPVOTE action works for postListReducer and root reducer", () => {
-    const postData = {
-      title: "How rare is this cat?",
-      text: "Cat found with 5 toes on each paw!",
-      timeSubmitted: 1672731107482,
-      upvotes: 0,
-      downvotes: 0,
-      score: 3,
-      id: 1,
-      imageUrl: "https://www.w3.org/Style/Woolly/woolly-mc.png",
+    const currentState = {
+      1: {
+        title: "How rare is this cat?",
+        text: "Cat found with 5 toes on each paw!",
+        timeSubmitted: 1672731107482,
+        upvotes: 0,
+        downvotes: 0,
+        score: 0,
+        id: 1,
+        imageUrl: "https://www.w3.org/Style/Woolly/woolly-mc.png",
+      },
+      2: {
+        title: "Puppy sitting in the snow",
+        text: "Adorable golden retriever puppy",
+        timeSubmitted: 1672731119244,
+        upvotes: 0,
+        downvotes: 0,
+        score: 0,
+        id: 2,
+        imageUrl: "https://img.freepik.com/free-photo/shot-adorable-white-golden-retriever-puppy-sitting-snow_181624-44122.jpg",
+      },
     };
-    const { title, text, timeSubmitted, upvotes, downvotes, score, id, imageUrl } = postData;
+    const { upvotes, score, id } = currentState[1];
     const action = {
       type: "UPVOTE",
-      title: title,
-      text: text,
-      timeSubmitted: timeSubmitted,
       upvotes: upvotes,
-      downvotes: downvotes,
       score: score,
       id: id,
-      imageUrl: imageUrl,
     };
     store.dispatch(action);
-    expect(store.getState().mainPostList).toEqual(postListReducer({}, action));
+    expect(store.getState().mainPostList).toEqual(postListReducer(currentState, action));
   });
 });
